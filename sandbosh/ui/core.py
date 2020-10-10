a6459c843e1572affa780936c337b1f49be59d2a
TakuKitamura <takukitamura.io@gmail.com>
2020-10-10 12:40:55 +0900

    add create_shell_line ui and logic

--- sandbosh/ui/core.py
+++ sandbosh/ui/core.py
@@ -2,21 +2,40 @@ import tkinter as tk
 from tkinter import ttk
 
 
-class ShellUI(tk.Frame):
+class ShellUI(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+
+        master.winfo_height
+
+        # ウィンドータイトル
+        self.master.title('SandBosh')
+
+        # 初期画面の大きさ
+        self.master.geometry('1000x600')
+
+        # 背景色
+        self.master.configure(background='black')
+
         self.pack()
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = ttk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        # コマンド入力用のラインを1行作成
+        self.create_shell_line()
 
-        self.quit = ttk.Button(self, text="QUIT", command=self.master.destroy)
-        self.quit.pack(side="bottom")
+    # コマンド入力用のラインを1行作成
+    def create_shell_line(self, i=1):
+        def callback(event):
+            print(self.input_line.get())
+            self.input_line.configure(state='readonly')
+            self.create_shell_line(i+1)
+
+        self.doller_mark = ttk.Label(self, text="$ ", font=("", 30))
+        self.doller_mark.grid(row=i, column=1)
+
+        self.input_line = ttk.Entry(self,  font=("", 30))
+        self.input_line.bind('<Return>', callback)
+        self.input_line.grid(row=i, column=2)
 
     def say_hi(self):
         print("hi there, everyone!")
