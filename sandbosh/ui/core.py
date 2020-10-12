508f50bddcb1d9d8a84e6367b725067c76b08c39
TakuKitamura <takukitamura.io@gmail.com>
2020-10-12 19:22:05 +0900

    auto scroll when you input command

--- sandbosh/ui/core.py
+++ sandbosh/ui/core.py
@@ -6,6 +6,7 @@ import math
 WIDTH = 1000
 HEIGHT = 600
 
+
 class ShellUI(tk.Frame):
     def __init__(self, root=None):
         super().__init__(root)
@@ -17,20 +18,22 @@ class ShellUI(tk.Frame):
         # 初期画面の大きさ
         self.root.geometry('{}x{}'.format(WIDTH, HEIGHT))
 
+        self.root.configure(background='black')
+
         self.font = tk.font.Font(family='Consolas', size=30, weight='normal')
 
         # 基本的なパーツを載せるキャンバス
         self.canvas = tk.Canvas(
             self.root, background='black', borderwidth=0, highlightthickness=0)
 
+        # キャンバスが更新されるタイミング
+        def canvas_configure(event):
+            self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+            self.canvas.yview_moveto(self.canvas.bbox('all')[3])
+
         # スクロールできるフレーム
         self.scrollable_frame = tk.Frame(self.root)
-        self.scrollable_frame.bind(
-            '<Configure>',
-            lambda e: self.canvas.configure(
-                scrollregion=self.canvas.bbox('all')
-            )
-        )
+        self.scrollable_frame.bind('<Configure>', canvas_configure)
 
         self.canvas.create_window(
             (0, 0), window=self.scrollable_frame, anchor='nw')
@@ -78,10 +81,10 @@ class ShellUI(tk.Frame):
 
         self.doller_mark = tk.Label(
             self.scrollable_frame, text='$', foreground='white', background='black', borderwidth=0, font=self.font)
-        self.doller_mark.grid(row=i, column=1, sticky=tk.N)
+        self.doller_mark.grid(row=i, column=1, sticky=tk.N, padx=0)
 
         self.input_line = tk.Text(
-            self.scrollable_frame, wrap=tk.CHAR, height=1, width=(WIDTH//self.font.measure('A'))+1, foreground='white', background='black',   insertbackground='white', borderwidth=0, highlightthickness=0, selectbackground='skyblue', selectforeground='black', takefocus=False, font=self.font, padx=0, pady=0, insertwidth=0, autoseparators=0)
+            self.scrollable_frame, wrap=tk.CHAR, height=1, width=(WIDTH//self.font.measure('A'))+1, foreground='white', background='black', borderwidth=0, highlightthickness=0, selectbackground='skyblue', selectforeground='black', takefocus=True, font=self.font, padx=0, pady=1, insertwidth=1, autoseparators=0)
 
         self.input_line.bind('<Return>', enter_key_handler)
         self.input_line.bind('<KeyRelease>', input_key_release_handler)
